702102459edcc444abfb9e785214c23b87ae6fd6
Mileto Di Marco <45310927+Mileto75@users.noreply.github.com>
2022-02-17 14:44:47 +0100

    Update selectie.js

--- js/selectie.js
+++ js/selectie.js
@@ -10,6 +10,16 @@ function initialise() {
     hdgTitle.textContent = `${newsProvider} - ${newCategory}`;
 }
 
+//adds click eventlistener to change flag colour on click
+function addClickEventToFlagList(elementsList) {
+
+    for(let i = 0; i < elementsList.length; i++) {
+
+        elementsList[i].addEventListener("click", changeFlagColor);
+        
+    }
+}
+
 //Gebruik deze functie wanneer er op een vlaggetje geklikt wordt
 function changeFlagColor() {
     if(this.className != "flag orange") {
